374f2da793cc73e2b425d2c2a9ccdd0bbf163d5a
Matt May <matthewbentonmay@gmail.com>
2015-01-29 22:13:42 -0600

    added validations

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,7 @@ class Person < ActiveRecord::Base
   enum gender: { male: 0, female: 1 }
   scope :female, -> { where(gender: genders[:female]) }
   scope :male, -> { where(gender: genders[:male]) }
+
+  validates :height, presence: true
+  validates :weight, presence: true
 end
