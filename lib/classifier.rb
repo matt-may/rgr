256754c3d43faf407c4013223d04e476bbc845f9
Matt May <matthewbentonmay@gmail.com>
2015-02-22 19:54:16 -0600

    added frac back in for clarity

--- lib/classifier.rb
+++ lib/classifier.rb
@@ -102,7 +102,8 @@ module BayesClassifier
     # +class_name+:: Specified class name
     #
     def class_probability(feature_values, class_name)
-      prob = feature_multiply(feature_values, class_name)
+      frac = 1.0 / classes.size
+      prob = frac * feature_multiply(feature_values, class_name)
 
       # Return the probability
       prob
